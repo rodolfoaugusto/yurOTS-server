2d56af8552e3ae2612382d8cacc0a53aa09e3a7f
rodolfoaugusto <rodolfoaugusto@hotmail.com.br>
2020-12-26 12:55:18 -0300

    Signature Update

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -1,10 +1,10 @@
 /*
- * VintageMMO, a free game server emulator 
- * Official Repository on Github <https://github.com/rodolfoaugusto/VintageMMO-GameServer>
+ * YurOTS, a free game server emulator 
+ * Official Repository on Github <https://github.com/rodolfoaugusto/yurOTS-server>
  * Copyright (C) 2020 - Rodi <https://github.com/rodolfoaugusto>
  * A fork of The Forgotten Server(Mark Samman) branch 1.2 and part of Nostalrius(Alejandro Mujica) repositories.
  *
- * The MIT License (MIT). Copyright © 2020 <VintageMMO>
+ * The MIT License (MIT). Copyright © 2020 <YurOTS>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
  * to deal in the Software without restriction, including without limitation 
  * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
